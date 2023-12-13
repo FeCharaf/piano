5bf6a389043150ca8278b7d726c25ee24d7d22ab
FeCharaf <felipecharaf@gmail.com>
2023-12-13 19:42:36 -0300

    piano funcionando

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,11 @@
- const pianoKeys = document.querySelectorAll('.piano-keys .key'); //selecionando todas keys juntas
+ const pianoKeys = document.querySelectorAll(".piano-keys .key"); //selecionando todas keys juntas
+
+ const playTune = (key) => {
+  let audio = new Audio(`tunes/${key}.wav`); // audio "a" por padrão
+  audio.play();
+ }
 
  pianoKeys.forEach(key => {
-   //chamando 
-   key.addEventListener("click", () => playTune(key.dataset.key))
-    console.log(key.dataset.key);
- })
+   //chamando a função playTune passando o valor da datakey como argumento
+   key.addEventListener("click", () => playTune(key.dataset.key));
+ });
