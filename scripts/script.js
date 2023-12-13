b5f35f47e0b4faaeeecdbbdd11963e775baecb69
FeCharaf <felipecharaf@gmail.com>
2023-12-13 19:54:54 -0300

    Teclado adicionado

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
  const pianoKeys = document.querySelectorAll(".piano-keys .key"); //selecionando todas keys juntas
 
  const playTune = (key) => {
-  let audio = new Audio(`tunes/${key}.wav`); // audio "a" por padrão
+  let audio = new Audio(`tunes/${key}.wav`); // passando a key baseado na tecla pressionada
   audio.play();
  }
 
@@ -9,3 +9,9 @@
    //chamando a função playTune passando o valor da datakey como argumento
    key.addEventListener("click", () => playTune(key.dataset.key));
  });
+
+ const pressedKey = (e) => {
+  playTune(e.key);
+ }
+
+ document.addEventListener("keydown", pressedKey);
